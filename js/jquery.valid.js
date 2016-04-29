2fb7b382c12ec9286d979b8571b7e84e10952737
Sebastian Czarnecki <sebcza@outlook.com>
2016-04-29 09:42:25 +0200

    Added fix for multiple filed and blocked submit button

--- js/jquery.valid.js
+++ js/jquery.valid.js
@@ -1,18 +1,34 @@
-(function ($) {
+(function($) {
     'use strict';
-        $.fn.validateText = function (ex) {
-            $(this).change(function () {
-                ex = new RegExp(ex);
-                if(ex.test($(this).val())){
-                    $(this).addClass("success");
-                    $(this).removeClass("error");
-                    $(this).parent().find("button[type='submit']").attr("disabled", false);
+    $.fn.validateText = function(ex) {
+        $(this).change(function() {
+            ex = new RegExp(ex);
+            if (ex.test($(this).val())) {
+                $(this).addClass("success");
+                $(this).removeClass("error");
+
             } else {
                 $(this).addClass("error");
                 $(this).removeClass("success");
-                 $(this).parent().find("button[type='submit']").attr("disabled", true);
+
             }
-        });           
-            return this;
+            var counterError = 0;
+            $(this).parent().find("input").each(function() {
+
+                if ($(this).hasClass("error")) {
+                    counterError++;
+                    console.log("test");
+                }
+            });
+            console.log(counterError);
+
+            if (counterError > 0) {
+                $(this).parent().find("button[type='submit']").attr("disabled", true);
+            }
+            else {
+                $(this).parent().find("button[type='submit']").attr("disabled", false);
+            }
+        });
+        return this;
     };
-}(jQuery));
+} (jQuery));
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function ($) {
     'use strict';
      $(function () {
-         $("#inputEmail").validateText(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
+         $("#inputEmail").validateText(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/);
+         $("#inputName").validateText(/^[A-Z]$/);
      });   
 }(jQuery));
